6293fd9ae4996bb18c59f65c6632bc94f2e7ae8b
Matheus Silverio <matheusphillipo@gmail.com>
2022-07-21 22:38:22 -0300

    Inicializando ambiente de dev Front

---
+++ pets_off_road/src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h1>Every Start has a Beginning</h1>
+    </div>
+  )
+}
+
+export default App
