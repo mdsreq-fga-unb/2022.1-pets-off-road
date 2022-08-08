aac5d7df0f1b8b0a39b0d80fbc2e7f561e8eeb30
LacerdaRenan <renanrodlacerda@gmail.com>
2022-08-07 22:26:40 -0300

    Conectando com banco e criando tabela USUARIO

--- pets-backend/src/user/repository/userRepo.ts
+++ pets-backend/src/user/repository/userRepo.ts
@@ -1,8 +1,21 @@
-import userDatabase from '../../repositories/userDatabaseConnection'
-import { USER } from '../entities/user.entity';
-
-export class USERDATABASE {
-    public async createUser(user: USER): Promise<USER> {
-        return await userDatabase.insert(user).table('USUARIO');
+import { Injectable } from '@nestjs/common';
+import connection from '../../repositories/userDatabaseConnection'
+import { CreateUserDto } from '../dto/create-user.dto';
+import { User } from '../entities/user.entity';
+class UserDatabase {
+    public async createUser(user: User): Promise<User> {
+        await connection.insert(user).table('USUARIO');
+        return await this.findOne(user.cpf);
+    }
+    public async findAll():Promise<User[]>{
+        return await connection.select('*').from('USUARIO');
+    }
+    public async findOne(cpf: number):Promise<User>{
+        const users = await connection.select('*').from('USUARIO').where({cpf:cpf});
+        for(let user of users ){
+            return user;
+        }
     }
 }
+
+export default new UserDatabase();
--- pets-backend/src/user/user.controller.ts
+++ pets-backend/src/user/user.controller.ts
@@ -4,7 +4,7 @@ import { CreateUserDto } from './dto/create-user.dto';
 
 @Controller('user')
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+  constructor(private userService: UserService) {}
 
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
@@ -16,18 +16,8 @@ export class UserController {
     return this.userService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
+  @Get(':cpf')
+  findOne(@Param('cpf') id: string) {
     return this.userService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: any) {
-    return this.userService.update(+id, updateUserDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.userService.remove(+id);
   }
 }
