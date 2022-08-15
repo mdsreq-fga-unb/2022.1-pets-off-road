d4d5a01da5a71b7bdd0954dcdd1d6a08d018f888
LacerdaRenan <renanrodlacerda@gmail.com>
2022-08-15 19:12:05 -0300

    Update: Project database to post and get routes
    Co-authored-by: Filipi <filipiffunb@gmail.com>

--- pets-backend/src/project/project.module.ts
+++ pets-backend/src/project/project.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ProjectService } from './project.service';
 import { ProjectController } from './project.controller';
+import { ProjectDatabase } from './repository/ProjectDatabase';
 
 @Module({
   controllers: [ProjectController],
-  providers: [ProjectService]
+  providers: [ProjectService, ProjectDatabase]
 })
 export class ProjectModule {}
