8a197d09842f66a2c4ff09a78ea612b9144cf625
LacerdaRenan <renanrodlacerda@gmail.com>
2022-08-14 23:46:21 -0300

    Update: Banco de dados
    Co-authored-by: Filipi <filipiffunb@gmail.com>

--- pets-backend/src/repositories/migrations/20220815014838_create_new_user.js
+++ pets-backend/src/repositories/migrations/20220815014838_create_new_user.js
@@ -3,19 +3,23 @@
  * @returns { Promise<void> }
  */
 exports.up = async function(knex) {
-  return await knex.schema.raw(`
-    CREATE TABLE usuario (
-        cpf              BIGINT       NOT NULL,
-        nome             VARCHAR(100) NOT NULL,
-        email            VARCHAR(50)  NOT NULL,
-        telefone         BIGINT       NOT NULL,
-        senha            TEXT         NOT NULL,
-        nivel_acesso     BOOLEAN      NOT NULL  DEFAULT   FALSE,
-        criado_em        DATETIME     NOT NULL  DEFAULT   CURRENT_TIMESTAMP,
-        modificado_em    DATETIME     NOT NULL  DEFAULT   CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-        CONSTRAINT USUARIO_PK PRIMARY KEY (cpf))
-        ENGINE InnoDB;
-  `)
+    const siglaEstados = ['MG', 'AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MT', 'MS', 'PA', 'PB', 'PR', 'PE', 'PI', 'RJ', 'RN', 'RS', 'RO', 'RR', 'SC', 'SP', 'SE', 'TO']
+    return await knex.schema.raw(`
+        CREATE TABLE user (
+            cpf              BIGINT                 NOT NULL,
+            nome             VARCHAR(100)           NOT NULL,
+            email            VARCHAR(50)            NOT NULL,
+            telefone         BIGINT                 NOT NULL,
+            uf               ENUM(${siglaEstados})  NOT NULL,
+            cidade           VARCHAR(50)            NOT NULL,
+            endereco         VARCHAR(200)           NOT NULL,
+            senha            TEXT                   NOT NULL,
+            nivel_acesso     BOOLEAN                NOT NULL  DEFAULT   FALSE,
+            criado_em        DATETIME               NOT NULL  DEFAULT   CURRENT_TIMESTAMP,
+            modificado_em    DATETIME               NOT NULL  DEFAULT   CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+            CONSTRAINT USUARIO_PK PRIMARY KEY (cpf))
+            ENGINE InnoDB;
+    `)
 };
 
 /**
@@ -23,5 +27,5 @@ exports.up = async function(knex) {
  * @returns { Promise<void> }
  */
 exports.down = async function(knex) {
-  return await knex.schema.raw(`DROP TABLE usuario`)
+  return await knex.schema.raw(`DROP TABLE user`)
 };
