8f5eaf80f7f02d24903d10dbc57d6a59e6d86ff2
LacerdaRenan <renanrodlacerda@gmail.com>
2022-08-15 23:17:51 -0300

    Create: Animal
    Co-authored-by: Filipi <filipiffunb@gmail.com>

---
+++ pets-backend/src/animal/animal.controller.ts
@@ -0,0 +1,18 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { AnimalService } from './animal.service';
+import { CreateAnimalDto } from './dto/create-animal.dto';
+
+@Controller('animal')
+export class AnimalController {
+  constructor(private readonly animalService: AnimalService) {}
+
+  @Post()
+  create(@Body() createAnimalDto: CreateAnimalDto) {
+    return this.animalService.create(createAnimalDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.animalService.findAll();
+  }
+}
---
+++ pets-backend/src/animal/animal.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@nestjs/common';
+import { CreateAnimalDto } from './dto/create-animal.dto';
+import { UpdateAnimalDto } from './dto/update-animal.dto';
+import { AnimalDatabase } from './repository/AnimalDatabase';
+
+@Injectable()
+export class AnimalService {
+  constructor(private AnimalRepository: AnimalDatabase){}
+  create(createAnimalDto: CreateAnimalDto) {
+    return this.AnimalRepository.createAnimal(createAnimalDto);
+  }
+
+  findAll() {
+    return this.AnimalRepository.listOfAnimals();
+  }
+}
